e8e19d656e34993a4e5d9b39190670c20c75a81b
George C. Privon <accounts5@privon.com>
2019-04-10 14:44:28 -0400

    script to create a default database

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Tools to create and manipulate a sqlite3 database with information on telescope and funding proposals.
 
 Before using, copy `config.rkt.example` to `config.rkt` and edit it with your desired database location.
-
-Using `update_database.rkt` to add new proposal entries.
+Then run `create_database.rkt` to create the sqlite3 file and create the `proposals` table.
+Then use `update_database.rkt` to add new proposal entries.
---
+++ create_database.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(require db)
+
+; create the database and create the table
+(define (createdb dbloc)
+  (write-string (string-append "Creating database " dbloc "\n"))
+  (define conn (sqlite3-connect #:database dbloc
+                                #:mode 'create))
+  (query-exec conn "CREATE TABLE proposals (ID INTEGER PRIMARY KEY,
+type TEXT NOT NULL,
+organization TEXT NOT NULL,
+solicitation TEXT NOT NULL,
+telescope TEXT DEFAULT '',
+orgpropID TEXT NOT NULL,
+PI TEXT NOT NULL,
+title TEXT NOT NULL,
+CoI TEXT NOT NULL,
+status TEXT NOT NULL,
+submitteddate TEXT NOT NULL,
+resultdate TEXT DEFAULT '')")
+  (disconnect conn)
+  (write-string (string-append "Database created at " dbloc "\n")))
+
+; load configuration file
+(require (file "config.rkt"))
+
+; make sure we can use the sqlite3 connection
+(cond (not (sqlite3-available?))
+      (error "Sqlite3 library not available."))
+
+; create the database and add the `proposals` table if it doesn't exist
+(if (not (file-exists? dbloc))
+    (createdb dbloc)
+    (write-string "Database exists. Exiting."))
